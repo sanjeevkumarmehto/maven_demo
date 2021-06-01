9cecfadd201f1f8635396f5d26eed702afb2da6a
sanjeev kumar <sanjeev00018@gmail.com>
2021-06-01 16:05:09 +0530

    First commit

---
+++ maven_demo/src/main/java/com/google/App.java
@@ -0,0 +1,9 @@
+package com.google;
+
+public class App {
+	
+	public String sample() {
+		return "sample";
+	}
+
+}
---
+++ maven_demo/src/test/java/com/google/AppTest.java
@@ -0,0 +1,15 @@
+package com.google;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		System.out.println("success");
+	}
+
+}
